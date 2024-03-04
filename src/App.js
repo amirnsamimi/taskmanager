e8a1d0ec1dca57e92d843abfe4d6442a9e53d27d
suguinamae <rabin1381@fmail.com>
2024-03-04 10:57:05 -0800

    profile done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import Profile from "./components/profile/profile";
 
 function App() {
   return (
     <div className="App">
-
+      <Profile fullName={"wildon wari"} userName={"wariwil"} />
     </div>
   );
 }
