362916df621ec47f7000717aeb252e9e0658d4a1
amirnsamimi <amirnsamimi@gmail.com>
2024-03-04 11:24:17 +0330

    miss staged fixed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-<<<<<<< HEAD
-function App() {
-  return <div className="App"></div>;
-=======
-import Reportwidget from "./components/Reportwidgct/Reportwidget"
+
 
 function App() {
   return (
     <div className="App">
-      <Reportwidget />
+
     </div>
   );
->>>>>>> Repostwidgct
+
 }
 
 export default App;
