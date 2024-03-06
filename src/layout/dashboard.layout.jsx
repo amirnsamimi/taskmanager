9b8a335fb7081fe0a9beee5c075b3cf1ee6c2b38
amirnsamimi <amirnsamimi@gmail.com>
2024-03-06 10:23:12 +0330

    update on first grid

--- src/layout/dashboard.layout.jsx
+++ src/layout/dashboard.layout.jsx
@@ -1,11 +1,15 @@
+import Menu from "../components/menu/Menu";
 import Profile from "../components/profile/profile";
+import ReportCard from "../components/reportCard/ReportCard";
 
 const Dashboard = () => {
 
 
 return(
 <div className="dashboard-container">
-<div><Profile /></div>
+<div><Profile fullName="sajjad khodabiamorz" userName="sajjad1379" />
+<Menu /><ReportCard amount={24} variable={"hour"}  />
+</div>
 <div>dsa</div>
 <div>123</div>
 </div>
