5e2830c128ed75971b7fe5b53a7fdf3918a72994
amirnsamimi <amirnsamimi@gmail.com>
2024-03-06 09:50:49 +0330

    update on grids

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-
+import Dashboard from "./layout/dashboard.layout";
+import "./style/dashboard.style.css"
 function App(){
   return (
     <div className="App">
-
+    <Dashboard />
     </div>
   );
   }
